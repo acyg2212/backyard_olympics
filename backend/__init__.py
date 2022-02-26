9d4cace01cd1a7071d0828878ea50c91e70e3e4e
Aaron Cygnarowicz <64909592+acyg2212@users.noreply.github.com>
2022-02-26 17:11:28 -0500

    added the user model

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,11 +1,47 @@
+import imp
 import os
 from flask import Flask
 from flask_cors import CORS
 from backend.config import Config
+from flask_wtf.csrf import CSRFProtect, generate_csrf
+from flask_login import LoginManager, current_user
+
+from backend.models import db, User
 
 app = Flask(__name__)
 
+login_manager = LoginManager(app)
+CSRFProtect(app)
 
-@app.route('/')
-def index():
-    return '<h1>This Works So Far</h1>'
+CORS(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
+@app.after_request
+def inject_csrf_token(response):
+    response.set_cookie('csrf_token',
+                        generate_csrf(),
+                        secure=True if os.environ.get('FLASK_ENV') else False,
+                        samesite='Strict' if os.environ.get(
+                            'FLASK_ENV') else None,
+                        httponly=True)
+    return response
+
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def react_root(path):
+    print("path", path)
+    if path == 'favicon.ico':
+        return app.send_static_file('favicon.ico')
+    return app.send_static_file('index.html')
+
+
+@app.route('/api/csrf/restore')
+def restore_csrf():
+    id = current_user.id if current_user.is_authenticated else None
+    return {'csrf_token': generate_csrf(), "current_user_id": id}
